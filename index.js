c195f1f78e6d834c078de815406066e21d9b8783
Hafiz-Al-Shams <hafizalshams1917@gmail.com>
2024-12-24 02:05:32 +0600

    search query using regex done

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -49,6 +49,23 @@ async function run() {
 
         app.get('/fixedFoods', async (req, res) => {
             const cursor = foodCollection.find().sort({ purchaseCount: -1 }).limit(6);
+            const result = await cursor.toArray();
+            res.send(result);
+        });
+
+
+        app.get('/foods/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) };
+            const result = await foodCollection.findOne(query);
+            res.send(result);
+        });
+
+
+        app.get('/searchFoods', async (req, res) => {
+            const searchText = req.query.search || '';
+            const query = { name: { $regex: searchText, $options: 'i' } };
+            const cursor = foodCollection.find(query);
             const result = await cursor.toArray();
             res.send(result);
         });
